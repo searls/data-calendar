dd5ff7d807da53657cd3bf2fc6435a40fe5fe381
Justin Searls <searls@gmail.com>
2011-06-01 20:22:16 -0400

    now it takes arbitrary data, a schema, and a stringy date formatter.

--- public/spec/data-calendar-spec.js
+++ public/spec/data-calendar-spec.js
@@ -1,11 +1,25 @@
 describe("DataCalendar", function() {
 	describe("~initialization", function() {
-		var CONTAINER_ID = "blarg",config;
+		var CONTAINER_ID = "blarg",
+				DATA = [
+					{pandaName: "Polly", pandaDate: "2011-01-03 23:59:00.0" },
+					{pandaName: "Peter", pandaDate: "2011-01-08 23:59:00.0" }					
+				],
+				SCHEMA = {
+					title: 'pandaName',
+					start: 'pandaDate'
+				},
+				config;
 		beforeEach(function() {
 			spyOn($.fn, "fullCalendar");
 
 			DataCalendar({
-				containerId: CONTAINER_ID
+				containerId: CONTAINER_ID,
+				data: DATA,
+				schema: SCHEMA,
+				dateFormatter: function(dateString) {
+					return dateString.substring(0,dateString.length - 2)
+				}
 			});
 			
 			config = $.fn.fullCalendar.mostRecentCall.args[0];
@@ -29,6 +43,27 @@ describe("DataCalendar", function() {
 		
 		it("turns on theme", function() {
 		  expect(config.theme).toBe(true);
+		});
+		
+		it("applies the schema to the title", function() {
+		  expect(config.events[0].title).toEqual(DATA[0].pandaName);
+			expect(config.events[1].title).toEqual(DATA[1].pandaName);
+		});
+		
+		it("attempts to convert the stringy dates to Date objects", function() {
+		  expect(config.events[0].start.constructor).toBe(Date);
+		});
+		
+		it("correctly converts the year", function() {
+		  expect(config.events[0].start.getFullYear()).toBe(2011);
+		});
+		
+		it("correctly converts the month", function() {
+		  expect(config.events[0].start.getMonth()).toBe(0);
+		});
+		
+		it("correctly converts the day", function() {
+		  expect(config.events[0].start.getDate()).toBe(3);
 		});
 	});
 });
--- public/src/data-calendar.js
+++ public/src/data-calendar.js
@@ -1,12 +1,20 @@
 (function(j,$) {
 	j.DataCalendar = function(config) {
+		var events = _(config.data).map(function(datum) {
+			return {
+				title: datum[config.schema.title], 
+				start: Date.parse(config.dateFormatter(datum[config.schema.start]))
+			};
+		});
+		
 		$('#'+config.containerId).fullCalendar({
 			theme: true,
 			header: {
 				left: 'prev,next today',
 				center: 'title',
 				right: 'month,basicWeek'
-			}
+			},
+			events: events
 		});
 	};
 })(window,jQuery);
